35a8bf3a70318a4f1f435a9bf31b0967d30e7426
suji5262 <suji5262@gmail.com>
2023-06-20 15:43:38 +0900

    :sparkles: Ex_11654 file add

---
+++ bronze/아스키_코드/Ex_11654.java
@@ -0,0 +1,13 @@
+package 아스키_코드;
+
+import java.util.Scanner;
+
+public class Ex_11654 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int ch = sc.next().charAt(0);
+
+        System.out.println(ch);
+    }
+}
